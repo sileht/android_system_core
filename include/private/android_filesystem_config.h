1f78f8f5cfa1d3230f51b3a4c264d03d4654d87c
Agustin Almansi <agustin.almansi@realvnc.com>
2015-11-23 11:31:33 +0000

    mirrorlink: Add new group mapping for net_bind_service capability.
    Change-Id: I37400caa1b5f9decac1540f3a42feef201edd1c4

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -103,6 +103,7 @@
 
 #define AID_RFS           3012  /* Remote Filesystem for peripheral processors */
 #define AID_RFS_SHARED    3013  /* Shared files for Remote Filesystem for peripheral processors  */
+#define AID_NET_BIND_SERVICE 3500 /* bind to ports under 1024 */
 
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
@@ -185,6 +186,8 @@ static const struct android_id_info android_ids[] = {
     { "inet",          AID_INET, },
     { "net_raw",       AID_NET_RAW, },
     { "net_admin",     AID_NET_ADMIN, },
+    { "net_bind_service",
+                       AID_NET_BIND_SERVICE, },
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
